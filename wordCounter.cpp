d6d4f26cba63c1a774894d81b803773751d1aed5
Sean Carstensen <seanlcarstensen@gmail.com>
2015-09-27 22:48:16 -0700

    style changes and comments

--- wordCounter.cpp
+++ wordCounter.cpp
@@ -1,16 +1,32 @@
 #include "wordCounter.h"
 
+///********************************
+///Function: Constructor
+///Task:   creates wordCounter object and prepares input and output files for use
+///Returns: nothing
+///********************************
 wordCounter::wordCounter()
 {
     inputFile.open("C:/Users/Sandy/Desktop/Data Structures/2701.txt");
     outputFile.open("C:/Users/Sandy/Desktop/Data Structures/2701output.txt");
 }
-
+///********************************
+///Function: Destructor
+///Task:   closes input and output files
+///Returns: nothing
+///********************************
 wordCounter::~wordCounter()
 {
     inputFile.close();
     outputFile.close();
 }
+///********************************
+///Function: writeOutputFile
+///Task:   Writes the data within the two maps into separate files
+/// 2701output.txt: alphabetical list of each word used within the input file along with the frequency of use
+/// 2701outputfrequency.txt: list of frequencies along with associated words, descending order
+///Returns: true if the outputFile stream was accessed successfully, false otherwise
+///********************************
 bool wordCounter::writeOutputFile()
 {
     int vectorSize;
@@ -22,14 +38,16 @@ bool wordCounter::writeOutputFile()
             outputFile << i->first << "\t\t\t" << i->second << endl;
         }
 
-        outputFile << endl << endl << endl;
-
         outputFile.close();
         outputFile.open("C:/Users/Sandy/Desktop/Data Structures/2701frequencyoutput.txt");
 
         outputFile << "Frequency Bank" << endl;
         for (map<int,vector<string> >::iterator i = frequencyCollection.end(); i != frequencyCollection.begin(); i--)
         {
+            if (i == frequencyCollection.end())
+            {
+                i++;    /// skip last entry since it contains garbage instead of a valid entry
+            }
             outputFile << i->first << endl;
             vectorSize = i->second.size();
             for (int n = 0; n < vectorSize; n++)
@@ -39,7 +57,7 @@ bool wordCounter::writeOutputFile()
                 {
                     outputFile << ", ";
                 }
-                if (n % 3)
+                if ( ((n+1) % 5 == 0) and n != (vectorSize - 1))
                 {
                     outputFile << endl;
                 }
@@ -52,6 +70,13 @@ bool wordCounter::writeOutputFile()
     cout << "There was an error opening the output file" << endl;
     return false;
 }
+///********************************
+///Function: fillWordCollection
+///Task: fills the wordCollection map with every word used in the input file along with the associated frequency
+/// get one string at a time from .txt file using >> operator
+/// format strings to remove capitalization and punctuation before checking against wordCollection map
+///Returns: true if inputFile is accessed successfully, false otherwise
+///********************************
 bool wordCounter::fillWordCollection()
 {
     string rawWord;
@@ -87,6 +112,11 @@ bool wordCounter::fillWordCollection()
     cout << "Error opening inputFile" << endl;
     return false;
 }
+///********************************
+///Function: fillFreqCollection
+///Task:   fills the frequencyCollection map using the frequencies stored in wordCollection map
+///Returns: nothing
+///********************************
 void wordCounter::fillFreqCollection()
 {
     string word;
