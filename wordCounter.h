8eb42537fcb045370ebc468b7ee65874fd888629
Sean Carstensen <seanlcarstensen@gmail.com>
2015-09-28 13:24:20 -0700

    more comments

--- wordCounter.h
+++ wordCounter.h
@@ -11,17 +11,17 @@ using namespace std;
 class wordCounter
 {
     public:
-        wordCounter();
-        virtual ~wordCounter();
-        bool writeOutputFile();
-        bool fillWordCollection();
-        void fillFreqCollection();
+        wordCounter();              /// constructor
+        virtual ~wordCounter();     /// destructor
+        bool writeOutputFile();     /// writes the contents of wordCollection and frequencyCollection to separate files
+        bool fillWordCollection();  /// fills wordCollection map with each word in inputFile along with frequency
+        void fillFreqCollection();  /// fills frequency collection using the data stored in word collection
     protected:
     private:
-        map<string, int> wordCollection;
-        map<int,vector<string> > frequencyCollection;
-        ifstream inputFile;
-        ofstream outputFile;
+        map<string, int> wordCollection;                /// map with words as key and frequency of use as value
+        map<int,vector<string> > frequencyCollection;   /// map with frequency as key and string vector of words as value
+        ifstream inputFile;                 /// input stream that will be used to populate wordCollection
+        ofstream outputFile;                /// output stream that will be used to write two output files
 };
 
 #endif // WORDCOUNTER_H
